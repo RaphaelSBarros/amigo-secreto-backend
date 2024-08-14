be8e95055580622e34dc4038997f3c3be9543104
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-14 16:30:32 -0300

    Listando todos os Grupos

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as auth from "../controllers/auth";
 import * as events from "../controllers/events";
+import * as groups from "../controllers/groups";
 
 const router = Router();
 
@@ -11,9 +12,12 @@ router.get("/ping", auth.validate, (req: any, res: any) =>
 );
 
 router.get("/events", auth.validate, events.getAll);
-router.get('/events/:id', auth.validate, events.getEvent)
-router.post('/events', auth.validate, events.addEvent)
-router.put('/events/:id', auth.validate, events.updateEvent)
-router.delete('/events/:id', auth.validate, events.deleteEvent)
+router.get("/events/:id", auth.validate, events.getEvent);
+router.post("/events", auth.validate, events.addEvent);
+router.put("/events/:id", auth.validate, events.updateEvent);
+router.delete("/events/:id", auth.validate, events.deleteEvent);
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll)
+router.get('/events/:id_event/groups/:id', auth.validate, groups.getGroup)
 
 export default router;
