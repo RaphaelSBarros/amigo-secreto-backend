9981cf73597dbe1c35b9d19fab1ec7dce7bf2a8b
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-22 09:28:41 -0300

    Preparando o código para produção

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import express from "express";
 import cors from "cors";
 import https from "https";
 import http from "http";
+import fs from "fs";
 import siteRoutes from "./routes/site";
-import adminRoutes from "./routes/admin"
+import adminRoutes from "./routes/admin";
 import { requestInterpector } from "./routes/utils/requestInterceptor";
 
 const app = express();
@@ -26,8 +27,13 @@ const runServer = (port: number, server: http.Server) => {
 
 const regularServer = http.createServer(app);
 if (process.env.NODE_ENV === "production") {
-  // TODO: configurar SSL
-  // TODO: rodar server na 80 e 443
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY as string),
+    cert: fs.readFileSync(process.env.SSL_CERT as string),
+  };
+  const secServer = https.createServer(options, app);
+  runServer(80, regularServer);
+  runServer(443, secServer)
 } else {
   const serverPort: number = process.env.PORT
     ? parseInt(process.env.PORT)
